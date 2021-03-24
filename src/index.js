b2c265655e9439ef58ab4464415d1de93ce73856
sokkanji <sskkanji@gmail.com>
2021-03-24 11:52:46 +0900

    7. 조건부 렌더링(조건부 연산자로 if-else 구문 인라인)

--- src/index.js
+++ src/index.js
@@ -20,17 +20,14 @@ class LoginControl extends React.Component {
 
   render() {
     const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
 
     return (
       <div>
         <Greeting isLoggedIn={isLoggedIn} />
-        {button}
+        { isLoggedIn 
+          ? <LogoutButton onClick={this.handleLogoutClick} />
+          : <LoginButton onClick={this.handleLoginClick} />
+        }
       </div>
     );
   }
