691ffd77ae7b8dd1ede8b1d997c49c6952e5a422
sokkanji <sskkanji@gmail.com>
2021-03-25 11:42:56 +0900

    7. 조건부 렌더링 (컴포넌트가 렌더링하는 것 막기)

--- src/index.js
+++ src/index.js
@@ -2,74 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class LoginControl extends React.Component {
+function WarningBanner(props){
+  if (!props.warn){
+    return null;
+  }
+  return (
+    <div>Warning!</div>
+  );
+}
+
+class Page extends React.Component {
   constructor(props){
     super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogOutClick = this.handleLogOutClick.bind(this);
-    this.state = { isLoggedin : false };
+    this.state={showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
   }
 
-  handleLoginClick() {
-    this.setState({
-      isLoggedIn : true
-    });
-  }
-
-  handleLogOutClick() {
-    this.setState({
-      isLoggedIn : false
-    });
+  handleToggleClick(){
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
   }
 
   render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    return (
+    return(
       <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        { isLoggedIn
-          ? <LogoutButton onClick={this.handleLogOutClick} />
-          : <LoginButton onClick={this.handleLoginClick} />
-        }
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
       </div>
-    )
+    );
   }
-}
-
-function UserGreeting(props) {
-  return <h1>Welcome back!!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please Sign Up</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  } else {
-    return <GuestGreeting />;
-  }
-}
-
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
 }
 
 ReactDOM.render(
-  <LoginControl />,
+  <Page />,
   document.getElementById('root')
 );
