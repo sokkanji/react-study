edbc4f3ad5a0b7943fa93031791b54f2df8b5669
sokkanji <sskkanji@gmail.com>
2021-04-02 17:06:19 +0900

    두 개의 다른 배열 생성 시, 동일한 key 사용

--- src/index.js
+++ src/index.js
@@ -2,26 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 
-function ListItem(props){
-  const value = props.value;
+function Blog(props){
+
+  const slidebar = (
+    <ul>
+      {props.posts.map((post) => 
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+
+  const content = props.posts.map((post) => 
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  
   return (
-    <li>{value}</li>
-  )
+    <div>
+      {slidebar}
+      <hr />
+      {content}
+    </div>
+  );
 }
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) => 
-    <ListItem value={number} />
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
-}
-
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <Blog posts={posts} />,
   document.getElementById('root')
 );
