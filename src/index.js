724dd94b4b1eb0117d45d1e8b3155eabf87d81af
sokkanji <sskkanji@gmail.com>
2021-04-02 16:52:23 +0900

    8. 리스트와 key

--- src/index.js
+++ src/index.js
@@ -1,42 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
 
-function WarningBanner(props){
-  if (!props.warn){
-    return null;
-  }
+function ListItem(props){
+  const value = props.value;
   return (
-    <div>Warning!</div>
+    <li>{value}</li>
+  )
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => 
+    <ListItem value={number} />
+  );
+  return (
+    <ul>{listItems}</ul>
   );
 }
 
-class Page extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={showWarning: true};
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick(){
-    this.setState(state => ({
-      showWarning: !state.showWarning
-    }));
-  }
-
-  render() {
-    return(
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
-}
+const numbers = [1, 2, 3, 4, 5];
 
 ReactDOM.render(
-  <Page />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
